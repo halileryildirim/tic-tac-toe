e1bb717a5ee8111893447e87ba5ed8a9675ebc36
halileryildirim <halileryildirim@gmail.com>
2023-09-15 15:25:55 +0300

    Added styling for gameboard

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Gameboard = (() => {
     let gameboard = {
-        squares:["X","O","X","O","O","O","X","O","X"]
+        squares:["","","","","","","","",""]
     }
 
     return {gameboard};
@@ -26,6 +26,7 @@ restart.addEventListener("click", () => {
 
 for (cell in gameArray) {
     const cells = document.createElement('div');
+    cells.setAttribute("id", "cells")
     cells.innerText = gameArray[cell];
     playBoard.appendChild(cells);
 }
