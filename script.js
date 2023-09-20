0ea96f7c5d1918268c3c968b6a2eab872c52ed40
halileryildirim <halileryildirim@gmail.com>
2023-09-20 17:54:59 +0300

    Added winnerDeclare module to apply win conditions and stop the game

--- script.js
+++ script.js
@@ -10,26 +10,63 @@ const Gameboard = (() => {
     return {gameboard};
 })();
 
-/*
-[0,1,2]
-[3,4,5]
-[6,7,8]
-[0,3,6] Win conditions
-[1,4,7]
-[2,5,8]
-[0,4,8]
-[2,4,6]
-*/
+const gameArray = Gameboard.gameboard.squares;
+
+const Player = (name, symbol) => {
+    this.name = name;
+    this.symbol = symbol;
+    return {name, symbol}
+};
+
+const winnerDeclare = (() => {
+
+    const winner = () => {
+
+        let combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+        let winner = false;
+
+        if(!gameArray.includes(" ")) {
+            console.log("It's a tie!")
+        }
+
+        for(combs in combinations) {
+            let indicator = combinations[combs];
+            
+            if(gameArray[indicator[0]]=="X" && gameArray[indicator[1]]=="X" && gameArray[indicator[2]]=="X"){
+                winner = true;
+                message.innerText = "X";
+
+            }
+            else if(gameArray[indicator[0]]=="O" && gameArray[indicator[1]]=="O" && gameArray[indicator[2]]=="O"){
+                winner = true;
+                message.innerText = "O";
+            }
+            else if(!gameArray.includes(" ") && !winner) {
+                message.innerText = "TIE"
+            } 
+        }
+        return winner;
+    }
+
+    return {winner};
+})();
 
 const game = (() => {
-    const gameArray = Gameboard.gameboard.squares;
     
     const start = () => {
         let round = 0;
-
         const PlayerX = Player(document.getElementById("playerX").value, "X");
         const PlayerO = Player(document.getElementById("playerO").value, "O");
 
+        window.addEventListener("click", ()=>{
+            winnerDeclare.winner();
+            if(message.innerText == "X"){
+                message.innerText = `${PlayerX.name} wins`;
+            }
+            else if(message.innerText == "O") {
+                message.innerText = `${PlayerO.name} wins!`;
+            }
+        });
         message.innerText = `${PlayerX.name} is starting first`;
 
         for(cells in gameArray) {
@@ -42,50 +79,52 @@ const game = (() => {
 
             restart.addEventListener("click", () => {
                 cell.innerText = " ";
-                gameArray[cell.id] = " ";
+                gameArray.fill(" ");
                 round = 0;
-                message.innerText = " Enter Players Name & Hit Start! ";
+                message.innerText = " Enter Player Names & Hit Start! ";
                 playBoard.replaceChildren();
             });
 
-            cell.addEventListener("click", () => {
-                if(gameArray[cell.id] == " ") {
-                    if(round % 2 == 0) {
-                        gameArray[cell.id] = PlayerX.symbol;
-                        cell.innerText = gameArray[cell.id];
-                        ++round;
-                        message.innerText =  `${PlayerO.name}'s turn`;
+                cell.addEventListener("click", () => {
+                    let condition = !winnerDeclare.winner();
+                    if(condition) {
+                        if(gameArray[cell.id] == " ") {
+                            if(round % 2 == 0) {
+                                gameArray[cell.id] = PlayerX.symbol;
+                                cell.innerText = gameArray[cell.id];
+                                ++round;
+                                message.innerText =  `${PlayerO.name}'s turn`;
+                            }
+        
+                            else if (round % 2 == 1) {
+                                gameArray[cell.id] = PlayerO.symbol;
+                                cell.innerText = gameArray[cell.id];
+                                ++round;
+                                message.innerText = `${PlayerX.name}'s turn`;
+                            }
+                    }
                     }
 
-                    else if (round % 2 == 1) {
-                        gameArray[cell.id] = PlayerO.symbol;
-                        cell.innerText = gameArray[cell.id];
-                        ++round;
-                        message.innerText = `${PlayerX.name}'s turn`;
-                    }
-                }
-            });
+                    
+                });
         };
     }
     return {start};
 })();
 
-const Player = (name, symbol) => {
-    this.name = name;
-    this.symbol = symbol;
-    return {name, symbol}
-};
 
-
+ 
 start.addEventListener("click", (event) => {
-    let validateX = document.getElementById("playerX").checkValidity();
-    let validateO = document.getElementById("playerO").checkValidity();
+    let valX = document.getElementById("playerX").checkValidity();
+    let valO = document.getElementById("playerO").checkValidity();
 
-    if(validateO && validateX) {
+    if(valX && valO) {
+        gameArray.fill(" "); 
+        playBoard.replaceChildren();
         event.preventDefault();
         game.start();
     }
-    else {message.innerText = "Enter Players Name First!"};
+    else {message.innerText = "Enter Player Names First!"};
     
 })
 
